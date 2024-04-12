497e27a9e7323795d1d55e02d7d94ea1e0db7d81
Samten Thinlay <samtenthinlay706@gmail.com>
2024-04-12 09:54:26 -0700

    assignment

--- CAP1_02230073.py
+++ CAP1_02230073.py
@@ -10,43 +10,48 @@
 # SOLUTION
 # Your Solution Score:49483
 # CSF101-CAP/input_3_cap1.txt
-################################# Read the input.txt file
-def read_input(type_file_name): # We define a fucntion "read_input"
+################################# 
+# Reading the input.txt file
+def read_input(input_3_cap1): # We define a fucntion "read_input"
     x = [] # We simply create an empty list
-    with open(type_file_name, 'r') as files: # For each line in the file
+    with open('input_3_cap1.txt', 'r') as files: # For each line in the file
         for y in files: # Split the lines into two parts using whitespace as the delimiter
-            contrast_picked, end_result = y.split() # Create a tuple containing the two parts and append it to the above list
-            x.append((contrast_picked, end_result)) # Create a tuple containing the values of 'contrast_picked' and 'end_result', and append it to the list
-    return x # Return the list containing 'contrast_picked' and 'end_result'
+            my_choice, end_result = y.split() # Create a tuple containing the two parts and append it to the above list
+            x.append((my_choice, end_result)) # Create a tuple containing the values of 'my_choice' and 'end_result', and append it to the list
+    return x # Return the list containing 'my_choice' and 'end_result'
 
 # Calculating the point for each game/round
-def calculate_score(Total_Matches_Competed): # Calculates a score based on "Total_Matches_Competed"
+def calculate_score(Total_Matches_Competed): # Calculate a score based on "Total_Matches_Competed"
     Point = 0 # The starting point for each round is 0
-    for contrast_picked, end_result in Total_Matches_Competed: # Iterate through each game in "Total_Matches_Competed" 
-# and reveal the values of 'contrast_picked' and 'end_result'
-        if end_result == 'X':  # We must be DEFEATED
-            if contrast_picked == 'A': # Contrast chose Rock
-                Point += 3 #  Rock previals over scissors
-            elif contrast_picked == 'B': # Contrast chose Paper
-                Point += 1  # Paper previals over rock 
-            elif contrast_picked == 'C': # Contrast chose Scissors
-                Point += 2 #  Scissors previals over paper
-        elif end_result == 'Y':  # We need to produce a DRAW
-            if contrast_picked == 'A': # He/She chose Rock
-                Point += 4  # Rock results in a tie against Rock
-            elif contrast_picked == 'B': # He/She chose Paper
-                Point += 5  # Paper results in a tie against Paper
-            elif contrast_picked == 'C': # He/She chose Scissors
-                Point += 6  # Scissors results in a tie against  Scissors
-        elif end_result == 'Z':  # We need to WIN
-            if contrast_picked == 'A': # He/She chose Rock
-                Point += 8  # Paper triumphs over Rock
-            elif contrast_picked == 'B': # He/She chose Paper
-                Point+= 9 # Scissors triumphs over Paper
-            elif contrast_picked == 'C': # He/She chose Scissors
-                Point += 7 # Rock triumphs over Scissor
-    print(f"The final accumulation of points is:{Point}") #Printing out the total sum of the points from the input file
+    for my_choice, end_result in Total_Matches_Competed: # Iterate through each game in "Total_Matches_Competed" 
+    # and reveal the values of 'my_choice' and 'end_result'
+        score = {'A' : 1, 'B': 2, "C": 3}
+        RESULT = {"X":0,"Y" :3, "Z" :6}
+        if end_result == 'X':  # we will loose
+            if my_choice == 'C': # my choice is scissor to loose the game 
+                Point += 3 #  sum of my choice score and result value of x
+            elif my_choice == 'A': # my choice is rock to loose the game
+                Point += 1  # sum of my choice score and result value of x
+            elif my_choice == 'B': # my choice is paper to loose the game
+                Point += 2 #  sum of my choice score and result value of x
+
+        elif end_result == 'Y':  # the game will be draw
+            if my_choice == 'A': # my choice is rock to draw the game
+                Point += 4  # sum of my choice score and result value of y
+            elif my_choice == 'B': # my choice is paper to draw the game
+                Point += 5  # sum of my choice score and result value of y
+            elif my_choice == 'C': # my choice is scissor to draw the game
+                Point += 6  # sum of my choice score and result value of y
+
+        elif end_result == 'Z':  # We will WIN the game 
+            if my_choice == 'B': # my choice is paper to win the game
+                Point += 8  # sum of my choice score and result value of z
+            elif my_choice == 'C': # my choice is scissor to win the game
+                Point+= 9 # sum of my choice score and result value of z
+            elif my_choice == 'A': # my choice is rock to win the game
+                Point += 7 # sum of my choice score and result value of z
+    print(f"The Total result of the given input is:{Point}") #Printing out the total result of the points from the input file
 
 # Running the program
-type_file_name = "CSF101-CAP/input_3_cap1.txt"  # The respected Index Number for the students
+type_file_name = "input_3_cap1.txt"  # the given input from the assignment 
 calculate_score(read_input(type_file_name)) # Calculate the score using the data obtained from reading the input file
